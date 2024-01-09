109d216779677fb1232675c9f242e0fca50ec6fb
Diego Perez <joce@iastate.edu>
2024-01-08 19:55:21 -0600

    part 1 done

--- solution.c
+++ solution.c
@@ -3,10 +3,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char *fileName = "in.txt";
+const char *fileName = "day2.txt";
 
+const int RED_MAX = 12, GREEN_MAX = 13, BLUE_MAX = 14;
 
-const int red = 12, green = 13, blue = 14;
+int findId(char *str) {
+  char *ptr = str;
+  while (*ptr) {
+    if (isdigit(*ptr)) {
+      return strtol(ptr, &ptr, 10);
+    } else {
+      ptr++;
+    }
+
+    if (*ptr == '\0') {
+      break;
+    }
+  }
+  return -1;
+}
+
+long findColor(char *inputString, char *color, int maxColor) {
+  char *currentPos = inputString;
+  long number = -1;
+
+  while ((currentPos = strstr(currentPos, color)) != NULL) {
+    // Check if the substring starts with a number
+    if (currentPos != inputString && isdigit(*(currentPos - 2))) {
+      // Extract the number
+      char *endptr;
+      number = strtol(currentPos - 3, &endptr, 10);
+
+      if (number > maxColor) {
+        return -1;
+      }
+
+      if (endptr != currentPos - 1) {
+        // Successfully extracted a number
+      }
+    }
+
+    // Move to the next position to avoid an infinite loop
+    currentPos++;
+  }
+
+  return number;
+}
 
 int main() {
   FILE *file;
@@ -22,20 +64,27 @@ int main() {
     return 1; // Exit with error code
   }
 
-
   int sum = 0;
   while (fgets(line, sizeof(line), file) != NULL) {
-    const char *ptr = line;
-    const char *substring = "Game";
+    char *ptr = line;
 
-    char *result = strstr(ptr, substring);
+    long red = findColor(ptr, "red", RED_MAX);
+    long green = findColor(ptr, "green", GREEN_MAX);
+    long blue = findColor(ptr, "blue", BLUE_MAX);
 
-    printf("%ld", result - ptr);
+    const int gameid = findId(ptr);
 
-      
-      
+    if (red == -1 || green == -1 || blue == -1) {
+      continue;
+    } else {
+      sum += gameid;
+    }
 
+    // printf("%d\n", gameid);
+    // printf("%ld", result - ptr);
   }
+
+  printf("Sum: %d\n", sum);
 
   // close the file
   fclose(file);
